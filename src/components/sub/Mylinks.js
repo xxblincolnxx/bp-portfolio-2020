1d074d88fc00cf1dd4be5aae40c526a53e37ccd8
Ben Prothe <beprothe@gmail.com>
2020-05-16 19:33:22 -0400

    removed direct dl for resume

--- src/components/sub/Mylinks.js
+++ src/components/sub/Mylinks.js
@@ -16,11 +16,6 @@ export default function Links () {
             <FaLinkedin color='rgb(189, 189, 189)' size={40} />
           </a>
         </li>
-        <li>
-          <a href='https://res.cloudinary.com/deodwfxkn/image/upload/v1589671341/portfolio/ben_prothe_resume_avrdah.pdf' title='Download Resume (pdf)' download>
-            <BsDownload color='rgb(189, 189, 189)' size={30} />
-          </a>
-        </li>
       </ul>
     </>
   )
