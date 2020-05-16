e0d920f8e45ab3115ebb3f7b3ba4f585644d2da8
Ben Prothe <beprothe@gmail.com>
2020-05-16 17:05:56 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../dist/Style.css'
+import '../dist/style.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // CONDITIONAL IMPORTS:
