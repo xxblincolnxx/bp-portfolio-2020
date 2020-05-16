890aea6b0e8cb3b8f8183fbcc83860f144dbcd1c
Ben Prothe <beprothe@gmail.com>
2020-05-16 16:44:54 -0400

    working on deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -19,10 +18,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    }),
-    new CopyPlugin([
-      { from: '_redirects' }
-    ])
+    })
   ],
   watch: true,
   devServer: {
